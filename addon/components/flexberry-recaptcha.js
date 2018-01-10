8b35aaa1f9481620315d64328a6968ad55fa131c
Semyon Volkov <volkovsema@gmail.com>
2018-01-10 15:20:25 +0500

    Add comments

--- addon/components/flexberry-recaptcha.js
+++ addon/components/flexberry-recaptcha.js
@@ -7,14 +7,38 @@ const { getOwner } = Ember;
 
 export default Ember.Component.extend({
 
+  /**
+    Component's CSS-classes names.
+  */
+
   classNames: ['flexberry-recaptcha'],
 
+  /**
+    Environment configuration.
+
+    @property appConfig
+    @type Object
+    @default undefined
+  */
   appConfig: undefined,
 
+  /**
+    ReCaptcha public (site) key. Obtained from env
+
+    @property sitekey
+    @type String
+    @default undefined
+  */
   sitekey: undefined,
 
+  /**
+    Tabindex for reCaptcha component.
+  */
   tabindex: Ember.computed.alias('tabIndex'),
 
+  /**
+    Renders ReCaptcha according to given properties.
+  */
   renderReCaptcha() {
     if (Ember.isNone(window.grecaptcha)) {
       Ember.run.later(() => {
@@ -39,12 +63,18 @@ export default Ember.Component.extend({
     }
   },
 
+  /**
+    Resets ReCaptcha.
+  */
   resetReCaptcha() {
     if (Ember.isPresent(this.get('widgetId'))) {
       window.grecaptcha.reset(this.get('widgetId'));
     }
   },
 
+  /**
+    Allows to use onSuccess action.
+  */
   successCallback(reCaptchaResponse) {
     let action = this.get('onSuccess');
     if (Ember.isPresent(action)) {
@@ -52,6 +82,9 @@ export default Ember.Component.extend({
     }
   },
 
+  /**
+    Allows to use onExpired action.
+  */
   expiredCallback() {
     let action = this.get('onExpired');
     if (Ember.isPresent(action)) {
@@ -61,8 +94,9 @@ export default Ember.Component.extend({
     }
   },
 
-  // Lifecycle Hooks
-
+  /**
+  Initializes DOM-related component's properties.
+  */
   didInsertElement() {
     this._super(...arguments);
 
