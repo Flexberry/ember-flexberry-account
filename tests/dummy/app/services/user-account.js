5cde769c00e15b831e42d44b8cf3e8e78d6a6785
DubrovinPavel <dubrovip@yandex.ru>
2018-03-12 14:58:47 +0500

    Fix JSHint

--- tests/dummy/app/services/user-account.js
+++ tests/dummy/app/services/user-account.js
@@ -11,7 +11,7 @@ export default UserAccountService.reopen({
 
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    return new Ember.RSVP.Promise((resolve, reject) => {
+    return new Ember.RSVP.Promise((resolve) => {
       resolve(re.test(String(username).toLowerCase()));
     });
   },
